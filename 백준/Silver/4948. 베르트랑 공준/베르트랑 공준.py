da6cd955a981059b293feaaf4c6585334ee66c7a
황치연 <119822911+inswal843@users.noreply.github.com>
2025-01-29 16:58:53 +0900

    [Silver II] Title: 베르트랑 공준, Time: 148 ms, Memory: 44464 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,18 +1,31 @@
 import sys
-import math
 input = sys.stdin.readline
-while 1:
+
+# 1) Read all queries first and find maximum n
+nums = []
+while True:
     n = int(input())
     if n == 0:
         break
-    else:
-        cnt = 0
-        for i in range(n+1, 2*n+1):
-            chk = True
-            for j in range(2, int(math.sqrt(i)+1)):
-                if i%j==0:
-                    chk = False
-                    break
-            if chk:
-                cnt+=1
-        print(cnt)
+    nums.append(n)
+max_n = max(nums)  # largest n
+
+# 2) Sieve up to 2*max_n
+limit = 2 * max_n
+is_prime = [True] * (limit + 1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(limit**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i*i, limit+1, i):
+            is_prime[j] = False
+
+# 3) Build prefix sums where prefix[i] = number of primes <= i
+prefix = [0] * (limit + 1)
+for i in range(1, limit + 1):
+    prefix[i] = prefix[i-1] + (1 if is_prime[i] else 0)
+
+# 4) For each n, output number of primes in (n, 2n]
+for n in nums:
+    # prime count in range (n, 2n] = prefix[2n] - prefix[n]
+    print(prefix[2*n] - prefix[n])
