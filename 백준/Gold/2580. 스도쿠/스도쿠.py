3feb84671f5e884b953dcbe0579c04bbf81aa5f7
황치연 <119822911+inswal843@users.noreply.github.com>
2025-02-26 15:43:35 +0900

    [Gold IV] Title: 스도쿠, Time: 1276 ms, Memory: 118204 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,32 +1,36 @@
 import sys
 input = sys.stdin.readline
 data = [list(map(int, input().split())) for _ in range(9)]
-data_T = list(map(list, zip(*data)))
-empty_cells = [(i, j) for i in range(9) for j in range(9) if data[i][j] ==0]
-def is_valid(x, y, num):
-    if num in data[x]:
+data_tran = list(map(list, zip(*data)))
+empty = []
+for i in range(9):
+    for j in range(9):
+        if data[i][j] == 0:
+            empty.append((i, j))
+# print(empty)
+def is_vaild(x, y, num):
+    global data, data_tran
+    if num in data[x] or num in data_tran[y]:
         return False
-    elif num in data_T[y]:
-        return False
-    else:
-        start_x, start_y = (x//3)*3, (y//3)*3
-        for i in range(3):
-            for j in range(3):
-                if data[start_x+i][start_y+j]==num:
-                    return False
+    x_start = x//3 * 3
+    y_start = y//3 * 3
+    for i in range(3):
+        for j in range(3):
+            if data[x_start+i][y_start+j] == num:
+                return False
     return True
-def back(idx):
-    if idx == len(empty_cells):
-        for row in data:
-            print(*row)
-        sys.exit(0)
+def back(cnt):
+    if cnt == len(empty):
+        for i in range(9):
+            print(*data[i])
+        exit(0)
     else:
-        x, y = empty_cells[idx]
+        x, y = empty[cnt]
         for i in range(1, 10):
-            if is_valid(x, y, i):
+            if is_vaild(x, y, i):
                 data[x][y] = i
-                data_T[y][x] = i
-                back(idx+1)
+                data_tran[y][x] = i
+                back(cnt+1)
                 data[x][y] = 0
-                data_T[y][x] = 0
+                data_tran[y][x] = 0
 back(0)
