1a2f833a2fb28bdc52e11ef1eb261fc0d3f280ef
황치연 <119822911+inswal843@users.noreply.github.com>
2025-02-26 17:14:41 +0900

    [Silver I] Title: 효율적인 해킹, Time: 13236 ms, Memory: 219016 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,14 +1,15 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 for _ in range(M):
     e, s = map(int, input().split())
     graph[s].append(e)
+# print(graph)
 def bfs(start):
     q = deque()
     visited = [False] * (N+1)
+    q.append(start)
     cnt = 0
     visited[start] = True
     for i in graph[start]:
@@ -21,15 +22,12 @@ def bfs(start):
             for i in graph[node]:
                 q.append(i)
     return cnt
-answers = []
+cnts = []
 for i in range(1, N+1):
-    answers.append((bfs(i), i))
-# print(answers)
-max_cnt, x = max(answers)
-ans = []
-for i in range(N):
-    a, b = answers[i]
-    if a == max_cnt:
-        ans.append(b)
-ans.sort()
-print(*ans)
+    cnts.append((bfs(i), i))
+# print(cnts)
+max_cnt, x = max(cnts)
+# print(max_cnt)
+for num, idx in cnts:
+    if num == max_cnt:
+        print(idx, end=" ")
