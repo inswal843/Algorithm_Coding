ddbe01e0fd69b2618f12c858911ddbf2f0b2641d
Chiyeon Hwang <119822911+inswal843@users.noreply.github.com>
2025-11-11 16:54:32 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        double result = Math.sqrt(n);
+        if (result % 1 == 0){
+            return (long)Math.pow(result+1, 2);
+        }
+        return -1;
+    }
+}
